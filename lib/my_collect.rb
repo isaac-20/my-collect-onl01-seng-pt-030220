307e60961c387e92179cf2609911e3cf991bb3a0
Isaac Smith <isaacsmith215@aol.com>
2020-03-14 04:44:45 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+bill = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+def my_collect(array)
+  names = []
+  i = 0
+  while i < array.length
+    names << yield(array[i])
+    i += 1
+  end
+  names
+end
 
-
+my_collect(bill) {|i| i.split(" ").first}
